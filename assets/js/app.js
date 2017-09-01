31eb91ecae3c534e76c4dd7d82e4bd27e5c7c474
Travo100 <Travo100@gmail.com>
2017-08-31 20:08:42 -0700

    Create user working, WIP for Update and Delete

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,8 @@ $("#submit-form").on("click", function(e){
     var name = $("#name").val().trim();
     var favMovie = $("#fav-movie").val().trim();
 
-    // Push the name and favMovie for someone to the database
-    database.ref().push({
+    // Push the name and favMovie for someone to the movies collection
+    database.ref("/movies").push({
         name: name,
         favMovie: favMovie
     });
@@ -20,4 +20,30 @@ $("#submit-form").on("click", function(e){
     $("#fav-movie").val("");
 
     alert("Item added!");
+});
+
+// 
+database.ref("/movies").on("child_added", function(snap){
+    var snapName = snap.val().name;
+    var snapFavMovie = snap.val().favMovie;
+    var snapId = snap.key;
+    var tableData = "<tr><td>"+snapId+"</td>";
+    
+    tableData += "<td>"+snapName+"</td>";
+    tableData += "<td>"+snapFavMovie+"</td>";
+    tableData += "<td><button class='btn btn-default edit' data-movie-id="+snapId+">Edit</button></td>";
+    tableData += "<td><button class='btn btn-danger delete' data-movie-id="+snapId+">Delete</button></td></tr>";
+    
+    $(".table").append(tableData);
+});
+
+$(document).on("click", ".edit", function(e){
+    e.preventDefault()
+    var buttonId = $(this).attr("data-movie-id");
+
+});
+
+$(document).on("click", ".delete", function(e){
+    e.preventDefault()
+    var buttonId = $(this).attr("data-movie-id");
 });
