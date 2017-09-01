48a48b7d98e9563979f776529f91beb1c9cc4cdb
Travo100 <Travo100@gmail.com>
2017-08-31 18:10:31 -0700

    Adding items to my fb database

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
 var database = firebase.database();
+
+$("#submit-form").on("click", function(e){
+
+    // prevent the form from submitting on default
+    e.preventDefault();
+
+    // get the name and favMovie values from the input fields
+    var name = $("#name").val().trim();
+    var favMovie = $("#fav-movie").val().trim();
+
+    // Push the name and favMovie for someone to the database
+    database.ref().push({
+        name: name,
+        favMovie: favMovie
+    });
+
+    //clear out the values of name and favMovie 
+    $("#name").val("");
+    $("#fav-movie").val("");
+
+    alert("Item added!");
+});
